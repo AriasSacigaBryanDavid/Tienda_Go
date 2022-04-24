a8c70624d904c089c4e20f81c20808736dc425b6
AriasSacigaBryanDavid <bariassaciga@gmail.com>
2022-04-23 20:37:00 -0500

    configuracion de registro de clientes

--- Controllers/Contactos.php
+++ Controllers/Contactos.php
@@ -98,7 +98,82 @@
             $data['identidades']=$this->model->getIdentidades();
             $this->views->getView($this,"clientes", $data);
         }
-        
+        public function listar_clientes(){
+            $data = $this->model->getClientes();
+            for ($i=0; $i<count($data); $i++){
+                if($data[$i]['estado'] ==1){
+                   $data[$i]['estado'] = '<span class="p-1 mb-2 bg-success text-white rounded">Activo</span>';
+                   $data[$i]['acciones']='<div>
+                   <button class="btn btn-primary mb-2" type="button" onclick="btnEditarCli('.$data[$i]['id'].');"><i class="fas fa-user-edit"></i></button>
+                   <button class="btn btn-danger" type="button" onclick="btnEliminarCli('.$data[$i]['id'].');"><i class="fas fa-user-slash"></i></button>
+                   </div>';
+                }else {
+                   $data[$i]['estado'] ='<span class="p-1 mb-2 bg-danger text-white rounded">Inactivo</span>';
+                   $data[$i]['acciones']='<div>
+                   <button class="btn btn-success" type="button" onclick="btnReingresarCli('.$data[$i]['id'].');"><i class="fas fa-recycle"></i></button>
+                   </div>';
+                }  
+            }
+            echo json_encode($data, JSON_UNESCAPED_UNICODE);
+            die();
+       }
+       public function registrar_cliente(){
+           $nombre=$_POST['nombre'];
+           $identidad=$_POST['identidad'];
+           $n_identidad=$_POST['n_identidad'];
+           $telefono=$_POST['telefono'];
+           $correo= $_POST['correo'];
+           $direccion=$_POST['direccion'];
+           $id=$_POST['id'];
+           if( empty($nombre) || empty($identidad) || empty($n_identidad) || empty($telefono) || empty($correo) || empty($direccion)){
+               $msg =array('msg' =>'Todo los campos son obligatorios','icono'=>'warning');
+           }else{
+               if ($id== "") {
+                   $data=$this->model->registarCliente($nombre, $identidad, $n_identidad, $telefono, $correo,$direccion);
+                   if($data == "ok") {
+                       $msg =array('msg' =>'Cliente registrado con éxito','icono'=>'success');
+                   }else if($data == "existe"){
+                       $msg =array('msg' =>'El cliente ya éxiste','icono'=>'warning');
+                   }else {
+                       $msg =array('msg' =>'Error al registrar el cliente','icono'=>'error');
+                   } 
+               }else {
+                   $data=$this->model->modificarCliente($nombre, $identidad, $n_identidad, $telefono, $correo, $direccion,$id);
+                   if($data == "modificado") {
+                       $msg =array('msg' =>'Cliente modificado con éxito','icono'=>'success');
+                   }else {
+                       $msg =array('msg' =>'Error al modificado el cliente','icono'=>'error');
+                   } 
+               }     
+           }
+           echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+           die();
+       }
+       public function editar_cliente(int $id){
+           $data = $this->model->editarCli($id);
+           echo json_encode($data, JSON_UNESCAPED_UNICODE);
+           die();
+       }
+       public function eliminar_cliente(int $id){
+           $data = $this->model->accionCli(0, $id);
+           if($data ==1){
+               $msg =array('msg' =>'Cliente dado de baja','icono'=>'success');
+           }else {
+               $msg =array('msg' =>'Error al eliminar el cliente','icono'=>'error');
+           }
+           echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+           die();
+       }
+       public function reingresar_cliente(int $id){
+           $data = $this->model->accionCli(1, $id);
+           if($data ==1){
+               $msg =array('msg' =>'Cliente reingresado con éxito','icono'=>'success');
+           }else {
+               $msg =array('msg' =>'Error al reingresar el cliente','icono'=>'error');
+           }
+           echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+           die();
+       }
 
 
     }
--- Models/ContactosModel.php
+++ Models/ContactosModel.php
@@ -5,6 +5,7 @@
         public function __construct(){
             parent::__construct();
         }
+        //conexion de SQl a proveedores
         public function getIdentidades(){
             $sql="SELECT * FROM identidades WHERE estado=1";
             $data =$this->selectAll($sql);
@@ -15,7 +16,6 @@
             $data= $this->selectAll($sql);
             return $data;
         }
-
         public function registarProveedor(string $nombre, string $id_identidad, string $n_identidad, string $telefono, string $correo, string $direccion){
             $this->nombre =$nombre;
             $this->id_identidad = $id_identidad;
@@ -40,7 +40,6 @@
             
             return $res;
         }
-
         public function modificarProveedor(string $nombre, string $id_identidad,string $n_identidad, string $telefono, string $correo, string $direccion, int $id){
             $this->nombre =$nombre;
             $this->id_identidad = $id_identidad;
@@ -59,13 +58,11 @@
                 }
             return $res;
         }
-
         public function editarPro(int $id){
             $sql = "SELECT * FROM proveedores WHERE id = $id";
             $data = $this->select($sql);
             return $data;
         }
-        
         public function accionPro(int $estado, int $id){
             $this->id=$id;
             $this->estado=$estado;
@@ -74,6 +71,66 @@
             $data = $this->save($sql, $datos);
             return $data;
         }
-
+        //conexion de SQl a clientes
+        public function getClientes(){
+            $sql="SELECT c.*, i.id AS id_identidad, i.nombre AS identidad FROM clientes c INNER JOIN identidades i ON c.id_identidad =i.id";
+            $data= $this->selectAll($sql);
+            return $data;
+        }
+        public function registarCliente(string $nombre, string $id_identidad, string $n_identidad, string $telefono, string $correo, string $direccion){
+            $this->nombre =$nombre;
+            $this->id_identidad = $id_identidad;
+            $this->n_identidad = $n_identidad;
+            $this->telefono =$telefono;
+            $this->correo=$correo;
+            $this->direccion =$direccion;
+            $verificar="SELECT * FROM clientes WHERE nombre = '$this->nombre'";
+            $existe=$this->select($verificar);
+            if (empty($existe)) {
+                $sql="INSERT INTO clientes(nombre, id_identidad, n_identidad, telefono, correo, direccion) VALUES (?,?,?,?,?,?)";
+                $datos= array($this->nombre, $this->id_identidad, $this->n_identidad, $this->telefono, $this->correo, $this->direccion);
+                $data=$this->save($sql, $datos);
+                    if ($data == 1){
+                        $res = "ok";
+                    }else{
+                        $res = "error";
+                    }
+            }else {
+                $res = "existe";
+            }
+            
+            return $res;
+        }
+        public function modificarCliente(string $nombre, string $id_identidad,string $n_identidad, string $telefono, string $correo, string $direccion, int $id){
+            $this->nombre =$nombre;
+            $this->id_identidad = $id_identidad;
+            $this->n_identidad = $n_identidad;
+            $this->telefono =$telefono;
+            $this->correo = $correo;
+            $this->direccion =$direccion;
+            $this->id=$id;
+            $sql="UPDATE clientes SET nombre=?,id_identidad=?, n_identidad=? , telefono=?, correo=?,direccion=? WHERE id=?";
+            $datos= array($this->nombre, $this->id_identidad, $this->n_identidad, $this->telefono,$this->correo, $this->direccion, $this->id);
+            $data=$this->save($sql, $datos);
+                if ($data == 1){
+                    $res = "modificado";
+                }else{
+                    $res = "error";
+                }
+            return $res;
+        }
+        public function editarCli(int $id){
+            $sql = "SELECT * FROM clientes WHERE id = $id";
+            $data = $this->select($sql);
+            return $data;
+        }
+        public function accionCli(int $estado, int $id){
+            $this->id=$id;
+            $this->estado=$estado;
+            $sql = "UPDATE clientes SET  estado =? WHERE id=?";
+            $datos = array($this->estado, $this->id);
+            $data = $this->save($sql, $datos);
+            return $data;
+        }
     }
 ?>
